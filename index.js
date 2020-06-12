72786d155c321c838adc88b18f1cb6a4d5b57b8b
Kelsey Florek <kelseyflorek22@gmail.com>
2020-06-12 11:36:06 -0500

    connected mongodb

--- index.js
+++ index.js
@@ -5,13 +5,15 @@
  */
  const express = require("express");
  const path = require("path");
+ const {MongoClient} = require("mongodb");
 
 /**
  * App Variables
  */
  const app = express();
  const port = process.env.PORT || "8000";
-
+ const mongo_uri = "mongodb://admin:admin@localhost:27017/?authSource=admin"
+ const client = new MongoClient(mongo_uri, { useNewUrlParser: true, useUnifiedTopology: true });
 /**
  *  App Configuration
  */
@@ -27,6 +29,7 @@
 /**
  * Server Activation
  */
+ client.connect();
  app.listen(port, () => {
   console.log(`Listening to requests on http://localhost:${port}`);
 });
